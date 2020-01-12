3963ef14fbc33fa92d6437af7c87a97bf9735489
jcoolu <jcoulombe_2123@email.ric.edu>
2020-01-11 21:53:10 -0500

    added comments

--- src/AnimationTester.java
+++ src/AnimationTester.java
@@ -7,7 +7,7 @@ import javax.swing.Timer;
 
 /**
 This program implements an animation that moves
-a car shape.
+a car shape. User enters how many cars will appear on screen
  */
 public class AnimationTester
 {
@@ -25,71 +25,83 @@ public class AnimationTester
 
         ArrayList<CarShape> carList = new ArrayList<CarShape>(); // ArrayList of car shapes
         System.out.println("How many cars would you like shown?");
-        int count = scan.nextInt();
-        Color color;//color of each car
-
-        int length = 720;
-
-        // assign a color to each car and creates a car (restricted to how many cars entered by user)
-        int num = 0;
-        for (int i = 0; i < count; i++) {
-            color = new Color(rand.nextFloat(), rand.nextFloat(),
-                    rand.nextFloat());
-            if(i%10!=0) {
-                carList.add(new CarShape(num * (CAR_WIDTH / 2) + 10,
-                        i * (CAR_WIDTH / 2) + 10, CAR_WIDTH, color));
-                num++;
+        int count = -1;
+        try {
+            count = Integer.parseInt(JOptionPane.showInputDialog(frame, "How many cars would you like shown?", null));
+            if(count <0) {
+                throw new NumberFormatException();
             }
-            else {
-                num = 0;
-            }
-            length = i * (CAR_WIDTH / 2) + 10;
+        }
+        catch (NumberFormatException e) {
+            Component panel = null;
+            JOptionPane.showMessageDialog(panel, "Error: Input was not valid integer", "Error", JOptionPane.ERROR_MESSAGE);
+            count = -1;
         }
 
-    System.out.println(length);
-        ShapeIcon icon = new ShapeIcon(carList,
-                width, height);
+        if(count != -1) {
+            Color color;//color of each car
 
-        final JLabel label = new JLabel(icon);
-        label.setPreferredSize(new Dimension(width, length));
+            int length = 720;
+
+            // assign a color to each car and creates a car (restricted to how many cars entered by user)
+            int num = 0;
+            for (int i = 0; i < count; i++) {
+                color = new Color(rand.nextFloat(), rand.nextFloat(),
+                        rand.nextFloat());
+                if (i % 10 != 0) {
+                    carList.add(new CarShape(num * (CAR_WIDTH / 2) + 10,
+                            i * (CAR_WIDTH / 2) + 10, CAR_WIDTH, color));
+                    num++;
+                } else {
+                    num = 0;
+                }
+                length = i * (CAR_WIDTH / 2) + 10;
+            }
+
+            ShapeIcon icon = new ShapeIcon(carList,
+                    width, height);
+
+            final JLabel label = new JLabel(icon);
+            label.setPreferredSize(new Dimension(width, length));
 // Now, you can add whatever you want to the container
-        JScrollPane scrPane = new JScrollPane(label);
-        scrPane.setPreferredSize(new Dimension(width, height));
-        final int DELAY = 5;
-        // Milliseconds between timer ticks
-        Timer t = new Timer(DELAY, new
-                ActionListener() {
-                    public void actionPerformed(ActionEvent event) {
+            JScrollPane scrPane = new JScrollPane(label);
+            scrPane.setPreferredSize(new Dimension(width, height));
+            final int DELAY = 5;
+            // Milliseconds between timer ticks
+            Timer t = new Timer(DELAY, new
+                    ActionListener() {
+                        public void actionPerformed(ActionEvent event) {
 
-                        for (int i = 0; i < carList.size(); i++) {
-                            //if i is even, it translate by 2
-                            if (i % 2 == 0 && i != 0) {
-                                carList.get(i).translate(2, 0);
+                            for (int i = 0; i < carList.size(); i++) {
+                                //if i is even, it translate by 2
+                                if (i % 2 == 0 && i != 0) {
+                                    carList.get(i).translate(2, 0);
+                                }
+                                //else it translates by 1
+                                else {
+                                    carList.get(i).translate(1, 0);
+                                }
+
+                                //if car's x is equal to icon width, reset back to beginning of screen
+                                if (carList.get(i).getX() == width) {
+                                    carList.get(i).setX(-CAR_WIDTH * (i + 1));
+                                }
+
+                                label.repaint();
+
                             }
-                            //else it translates by 1
-                            else {
-                                carList.get(i).translate(1, 0);
-                            }
-
-                            //if car's x is equal to icon width, reset back to beginning of screen
-                            if (carList.get(i).getX() == width) {
-                                carList.get(i).setX(-CAR_WIDTH * (i + 1));
-                            }
-
-                            label.repaint();
-
                         }
-                    }
 
-                });
-        t.start();
+                    });
+            t.start();
 
-        frame.setLayout(new FlowLayout());
-        frame.setResizable(false);
-        frame.add(scrPane);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-        //   MoveableShape test = new MoveableShape();
+            frame.setLayout(new FlowLayout());
+            frame.setResizable(false);
+            frame.add(scrPane);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+            //   MoveableShape test = new MoveableShape();
+        }
     }
 }//end of class        
